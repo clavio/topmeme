792006aad1f0632580813f197db18928087cd4ab
6M21JH2\jbarra <relicorb@gmail.com>
2018-05-14 17:02:38 -0400

    Added Firebase Auth

--- app/src/main/java/com/ciphra/jbarra/topmeme/MainActivity.java
+++ app/src/main/java/com/ciphra/jbarra/topmeme/MainActivity.java
@@ -2,12 +2,35 @@ package com.ciphra.jbarra.topmeme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        if(currentUser == null){
+            Log.i("null", "null");
+        }
+        else{
+
+        }
     }
 }
